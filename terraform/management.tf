b75a67d5c1f7bee342b87a4d3a49a051c5e0b87d
Luca Dario <l.dario@zero12.it>
2024-11-11 16:42:33 +0100

    Change OU

--- terraform/management.tf
+++ terraform/management.tf
@@ -1,7 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-module "audit_accont" {
+module "management_accont" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
